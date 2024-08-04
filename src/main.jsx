900409ff8d6a533e535f8757685c5b8926e3b514
codeitownit <gachuieve32@gmail.com>
2024-08-03 21:08:00 -0700

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -15,38 +15,17 @@
 //    </React.StrictMode>,
 // )
 
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-// import APP from './APP';
+import APP from './APP';
 
-// import './index.css';
-
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <APP/>
-//   </React.StrictMode>,
-// )
-
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./APP/Modules/Dashboard/Pages/Login";
-import Dashboard from "./APP/Modules/Dashboard";
+import './index.css';
 
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />}>
-          <Route index element={<Login />} />
-          <Route path="dashboard" element={<Dashboard />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <APP/>
+  </React.StrictMode>,
+)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
